64ff668d1db7ac27f6977db51c9fbc8c3001cd76
Nitish Amraji <nitishamraji@gmail.com>
2024-07-20 17:50:35 -0500

    Update SchwabMarketDataService.js

--- server/services/SchwabMarketDataService.js
+++ server/services/SchwabMarketDataService.js
@@ -21,6 +21,22 @@ class SchwabMarketData {
             throw error;
         }
     }
+
+    async getQuote(symbol) {
+        const quotesUrl = `https://api.schwabapi.com/marketdata/v1/${symbol}/quotes?fields=quote%2Creference&indicative=false`;
+
+        try {
+            const response = await axios.get(quotesUrl, {
+                headers: {
+                    'Authorization': `Bearer ${this.accessToken}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching quotes:', error.response ? error.response.data : error.message);
+            throw error;
+        }
+    }
 }
 
 // Export the class
