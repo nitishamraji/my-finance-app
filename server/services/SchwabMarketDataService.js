7877bf0676be669998b884513d2d67628c87f668
Nitish Amraji <nitishamraji@gmail.com>
2024-07-20 21:07:14 -0500

    Update SchwabMarketDataService.js

--- server/services/SchwabMarketDataService.js
+++ server/services/SchwabMarketDataService.js
@@ -23,7 +23,7 @@ class SchwabMarketData {
     }
 
     async getHistoryData(symbol, queryParams) {
-        const quotesUrl = `https://api.schwabapi.com/marketdata/v1/${symbol}/pricehistory?{queryParams}`;
+        const quotesUrl = `https://api.schwabapi.com/marketdata/v1/${symbol}/pricehistory?${queryParams}`;
         let accessToken = await schwabAuth.getAccessToken();
         
         try {
